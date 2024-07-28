4ff4e68ecfb6c94b4be11c9f5f20b9667625c52c
anandromy <romyanand2004@gmail.com>
2024-07-28 08:15:15 +0530

    mistake correction of if condition-user.isVerified to to tell user to verify

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -36,7 +36,7 @@ export const authOptions: NextAuthOptions = {
                         throw new Error("No user found with this email")
                     }
 
-                    if(user.isVerified) {
+                    if(!user.isVerified) {
                         throw new Error("Please verify your account before login")
                     }
                     const isPasswordCorrect = await bcrypt.compare(credentials.password, user.password)
